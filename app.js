191921185a522074b5872558cc75b37e309aaf26
Niro-jpg <nicolodentale@gmail.com>
2024-06-22 19:30:33 +0200

    added express

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Moblando!');
-}).listen(8000); 
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('index.html', function(err, data) {
+    if (err) {
+      res.end('Errore nella lettura del file');
+    } else {
+      res.end(data);
+    }
+  });
+}).listen(8000);
