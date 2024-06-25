dcd88aa56d39582bd6b859e408e7f19b27e348c6
Niro-jpg <nicolodentale@gmail.com>
2024-06-25 12:29:46 +0200

    changed design

--- public/javascript/advises.js
+++ public/javascript/advises.js
@@ -1,5 +1,23 @@
 function send_advise() {
     var input = document.getElementById("advise_input");
-    alert("Il valore inserito è: " + input.value);
+
+    var params = {
+        name: "marameo",
+        email: "nicolodentale@gmail.com",
+        subject: "miao",
+        message: input.value,
+      };
+
+    emailjs.send('service_uv3x29g', 'template_gbug8uh', params)
+        .then(res=>{
+            input.value = ""
+            console.log(res);
+            alert("Your message sent successfully!!")
+        })
+        .catch(err=>console.log(err));
     input.value = ""
+}
+
+function go_to_homepage() {
+    window.location.href = "/";
 }
